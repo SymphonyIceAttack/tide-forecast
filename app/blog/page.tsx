c33f1c458a3c561c8b02fba67b8e96e151cb453d
SymphonyIceAttack <1994370030@qq.com>
2025-11-04 19:49:11 +0800

    feat: metadata blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ import {
 import directus from "@/lib/directus";
 
 export const metadata: Metadata = {
-  title: "Blog - Fancy Text Generator",
+  title: "Blog - Tide Forecast",
   description: "Read our latest articles and updates",
 };
 
