3ecc5c8d877255a6e667c9e275e9085d92ae4a5c
SymphonyIceAttack <1994370030@qq.com>
2025-11-09 09:47:27 +0800

    feat: blog image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "symcloud.top",
+      },
+      {
+        protocol: "https",
+        hostname: "**", // Allow all HTTPS domains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
